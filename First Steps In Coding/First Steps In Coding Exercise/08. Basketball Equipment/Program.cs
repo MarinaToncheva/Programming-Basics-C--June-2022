7295546dece234a71c8cadfea86db54d847844e5
Marina Toncheva <mar.toncheva@gmail.com>
2022-12-18 15:37:34 +0200

    Exercise

---
+++ First Steps In Coding/First Steps In Coding Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearTax = int.Parse(Console.ReadLine());
+            double sneakers = yearTax - (yearTax * 0.40);
+            double outfit = sneakers - (sneakers * 0.20);
+            double ball = outfit / 4;
+            double accessories = ball / 5;
+            double allExpenses = yearTax + sneakers + outfit + ball + accessories;
+            Console.WriteLine(allExpenses);
+        }
+    }
+}
