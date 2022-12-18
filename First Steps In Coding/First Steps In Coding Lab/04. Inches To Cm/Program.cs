e571cc0693a9f6a3d9a277cc684f5cfcaca140d9
Marina Toncheva <mar.toncheva@gmail.com>
2022-12-18 15:52:50 +0200

    Lab & Exercise

---
+++ First Steps In Coding/First Steps In Coding Lab/04. Inches To Cm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04._Inches_To_Cm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double cm = inches * 2.54;
+            Console.WriteLine(cm);
+        }
+    }
+}
